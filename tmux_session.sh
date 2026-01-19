787082dd6ee412f3faa066255f6b3c516ba383aa
Undeadamien <damien.trala@outlook.com>
2026-01-19 12:30:47 +0100

    improved "tmux_session.sh"

--- tmux_session.sh
+++ tmux_session.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 0
 
 NAME_SESSION="artref"
+PROJECT_DIR="$(pwd)"
 
 if tmux has-session -t "$NAME_SESSION"; then
     tmux kill-session -t "$NAME_SESSION"
@@ -19,8 +20,11 @@ tmux new-session -d -s "$NAME_SESSION"
 tmux set-option -t "$NAME_SESSION" base-index 1
 tmux set-option -t "$NAME_SESSION" pane-base-index 1
 
-tmux set-hook -t "$NAME_SESSION" after-new-window "send-keys \"source $(pwd)/.venv/bin/activate\" Enter \"clear\" Enter"
-tmux set-hook -t "$NAME_SESSION" after-split-window "send-keys \"source $(pwd)/.venv/bin/activate\" Enter \"clear\" Enter"
-tmux send-keys -t "$NAME_SESSION:1.1" "source $(pwd)/.venv/bin/activate" Enter "clear" Enter
+tmux set-hook -t "$NAME_SESSION" after-new-window[0] "send-keys 'source ${PROJECT_DIR}/.venv/bin/activate' Enter"
+tmux set-hook -t "$NAME_SESSION" after-new-window[1] "send-keys 'clear' Enter"
+tmux set-hook -t "$NAME_SESSION" after-split-window[0] "send-keys 'source ${PROJECT_DIR}/.venv/bin/activate' Enter"
+tmux set-hook -t "$NAME_SESSION" after-split-window[1] "send-keys 'clear' Enter"
+
+tmux send-keys -t "$NAME_SESSION:1.1" "source ${PROJECT_DIR}/.venv/bin/activate" Enter "clear" Enter
 
 tmux attach -t "$NAME_SESSION:1.1"
